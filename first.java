8888c2342c24f4096dac4e6c3156243077d78a86
akanksha230802 <145675079+akanksha230802@users.noreply.github.com>
2023-09-21 05:06:48 -0700

    Add files via upload

---
+++ first.java
@@ -0,0 +1,16 @@
+ public class first{
+public static void main(String[] args) {
+    int a=2;
+    int b=4;
+    int sum=a+b;
+System.out.println(sum);
+int x = 'U';
+System.out.println("ASCII value of 'U' is:"+x);
+double l=4.0;
+double h =6.0;
+double  area=l*h;
+System.out.println("Area:"+area);
+int num=3;
+System.out.println("the cube of 3 is:"+num*num*num);
+}
+ }
